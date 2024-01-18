ebc46dae84522c81e70b4a640f0bb9a94c3aa90d
Ana Paula Vizcaya Hernandez <avizcaya@dunegpvm03.fnal.gov>
2024-01-18 14:19:12 -0600

    Changes to include run conditions service

---
+++ dunecalib/CalibServices/RunConditionsService.h
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////
+// RunConditionsService.h
+//
+// Pure virtual service interface for run conditions functions
+//
+//  avizcaya@colostate.edu
+//  date: Jan 19, 2024
+//
+////////////////////////////////////////////////////////////////////////
+#ifndef RUNCONDITIONSSERVICE_H
+#define RUNCONDITIONSSERVICE_H
+
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "dunecalib/Calib/LifetimeCalib.h"
+#include "larcore/CoreUtils/ServiceUtil.h"
+
+namespace runc{
+    class RunConditionsService {
+      public:
+      typedef runc::RunConditions provider_type;
+
+      public:
+      virtual ~RunConditionsService() = default;
+
+      virtual void   reconfigure(fhicl::ParameterSet const& pset) = 0;
+      virtual runc::RunConditions* provider() const = 0;
+
+      }; // class RunConditionsService
+    } //namespace detinfo
+DECLARE_ART_SERVICE_INTERFACE(runc::RunConditionsService, LEGACY)
+#endif // RUNCONDITIONSSERVICE_H
---
+++ dunecalib/CalibServices/RunConditionsServiceProtoDUNE_service.cc
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////
+// \file RunConditionsProtoDUNE_service.cc
+//
+// \brief implementation of class for storing/accessing run conditions parameters for ProtoDUNE
+//
+// \author avizcaya@colostate.edu
+// \date Jan 19, 2024
+// 
+////////////////////////////////////////////////////////////////////////
+
+// C++ language includes
+#include <iostream>
+
+#include "TTimeStamp.h"
+
+// LArSoft includes
+#include "dunecalib/CalibServices/RunConditionsServiceProtoDUNE.h"
+
+// Framework includes
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "cetlib_except/exception.h"
+
+//-----------------------------------------------
+runc::RunCondtitionsServiceProtoDUNE::RunConditionsServiceProtoDUNE(fhicl::ParameterSet const& pset, art::ActivityRegistry &reg)
+{
+  std::cout << "Service funtion 1" << std::endl;
+  //fProp.reset(new calib::LifetimeCalibProtoDUNE(pset));
+
+  //reg.sPreBeginRun.watch(this, &LifetimeCalibServiceProtoDUNE::preBeginRun);
+}
+
+//----------------------------------------------
+void runc::RunConditionsServiceProtoDUNE::preBeginRun(const art::Run& run)
+{
+  art::Timestamp ts = run.beginTime();
+  TTimeStamp tts(ts.timeHigh(), ts.timeLow());
+  uint64_t  runtime = tts.AsDouble();
+  
+  std::cout << "db: runtime in runs conditions " << runtime << std::endl;
+  std::cout << "El numero de run es: " << run.id() << std::endl;
+  // one can also consider using event time through "sPreProcessEvent" by define a "preProcessEvent" function, for exmaple.
+  
+  //fProp->Update(runtime);
+
+  //fProp->Update(run.id().run());
+}
+
+//------------------------------------------------
+void runc::RunConditionsServiceProtoDUNE::reconfigure(fhicl::ParameterSet const& pset)
+{
+  std::cout << "Esto pasa?" << std::endl;
+  //fProp->Configure(pset);  
+  return;
+}
+
+//------------------------------------------------
+DEFINE_ART_SERVICE_INTERFACE_IMPL(runc::RunConditionsServiceProtoDUNE, runc::RunConditionsService)
